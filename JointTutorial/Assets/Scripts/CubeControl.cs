b6302e207800024aa475d37b1169584162d776eb
samiyagmur <samiyagmur92@gmail.com>
2022-05-28 23:40:42 +0300

    SolitionCollision

--- JointTutorial/Assets/Scripts/CubeControl.cs
+++ JointTutorial/Assets/Scripts/CubeControl.cs
@@ -6,32 +6,68 @@ public class CubeControl : MonoBehaviour
 {   
    
     [SerializeField] private float speed = 10;
-    public PhysicMaterial material;
+    bool IsCollision;
     Rigidbody rb;
+    float force=5f;
+    float time;
     private void OnCollisionEnter(Collision collision)
     {
-        
-      
-        
-    }
-    private void Start()
-    {   
-       
-        material.bounciness = 1f;
-        
-        rb= GetComponent<Rigidbody>();
-    }
+        Debug.Log("girdi");
+        IsCollision = true;
 
+        float _delayTime
+        //speed = 0;
+        //Vector3.Lerp(new Vector3(0, 0, -3.17993f), new Vector3(0, 0, -15f), 10);
+        //speed = 10;
+        //rb.MovePosition(new Vector3(0, 0, -15f)) ;
+        //rb.AddForce(new Vector3(0, 0, -3.17993f), ForceMode.Impulse);
+        
+        //rb.velocity = Vector3.back * 10;
+
+
+    }
+   
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (transform.position.z==-15)
+        {
+
+        }
+
+    }
     private void Update()
     {
-            
-            
+        Debug.Log(IsCollision);
 
-            
-            Debug.Log("girdi");
+
+
+        if (IsCollision)
+        {
+
+            if (transform.position.z >= -15f)
+            {
+                speed = 0;
+                rb.AddForce(new Vector3(0, 0, -force), ForceMode.Impulse);
+            }
+            //IsCollision = false;
+            //force = 0;
+        }
+        
+
+
+
             //transform.Translate(Vector3.up * Time.deltaTime * speed* Input.GetAxis("Vertical"));
             //transform.Translate(Vector3.forward * Time.deltaTime * speed /** Input.GetAxis("Horizontal")*/);
             rb.velocity = Vector3.forward*speed;
+            
     }
+
+    void TimeDelay()
+    {
+        
+    }
+    
 
 }
