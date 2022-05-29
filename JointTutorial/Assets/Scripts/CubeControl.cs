2c5be1a0772b90d9a78e58308db43245b098a463
samiyagmur <samiyagmur92@gmail.com>
2022-05-29 12:38:30 +0300

    BounceForever

--- JointTutorial/Assets/Scripts/CubeControl.cs
+++ JointTutorial/Assets/Scripts/CubeControl.cs
@@ -8,66 +8,49 @@ public class CubeControl : MonoBehaviour
     [SerializeField] private float speed = 10;
     bool IsCollision;
     Rigidbody rb;
-    float force=5f;
-    float time;
+    float force = 3f;
+    
+    
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("girdi");
         IsCollision = true;
-
-        float _delayTime
-        //speed = 0;
-        //Vector3.Lerp(new Vector3(0, 0, -3.17993f), new Vector3(0, 0, -15f), 10);
-        //speed = 10;
-        //rb.MovePosition(new Vector3(0, 0, -15f)) ;
-        //rb.AddForce(new Vector3(0, 0, -3.17993f), ForceMode.Impulse);
-        
-        //rb.velocity = Vector3.back * 10;
-
-
+        //force = 5f;
     }
-   
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-
-        if (transform.position.z==-15)
-        {
-
-        }
-
+        
+        rb =GetComponent<Rigidbody>();
     }
-    private void Update()
+    private void FixedUpdate()
     {
-        Debug.Log(IsCollision);
+        
 
-
-
-        if (IsCollision)
+        if (IsCollision )
         {
-
-            if (transform.position.z >= -15f)
+            
+            if (transform.position.z>=-15f)
             {
                 speed = 0;
                 rb.AddForce(new Vector3(0, 0, -force), ForceMode.Impulse);
             }
-            //IsCollision = false;
-            //force = 0;
+            else
+            {
+                IsCollision=false;
+
+                Invoke("TimeDelay", 3);
+            }
         }
-        
-
-
-
-            //transform.Translate(Vector3.up * Time.deltaTime * speed* Input.GetAxis("Vertical"));
-            //transform.Translate(Vector3.forward * Time.deltaTime * speed /** Input.GetAxis("Horizontal")*/);
-            rb.velocity = Vector3.forward*speed;
-            
+        else
+        {
+            rb.velocity = Vector3.forward * speed;
+        }
     }
-
     void TimeDelay()
     {
-        
+        speed = 10f;
     }
+      
+   
     
 
 }
