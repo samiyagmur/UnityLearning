b954c8e3083cf90d13e95a5df3738d79ecbfece0
samiyagmur <samiyagmur92@gmail.com>
2022-06-23 21:22:19 +0300

    new

--- InputSystem/Assets/Scripts/PlayerControl.cs
+++ InputSystem/Assets/Scripts/PlayerControl.cs
@@ -5,28 +5,62 @@ using UnityEngine.InputSystem;
 
 
 public class PlayerControl : MonoBehaviour
-{   
-    Rigidbody rb;
+{
+    #region private veriables
 
+    Rigidbody rb;
+    PlayerInput playerInput;
+
+    #endregion
+
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+        
+        PlayerInputActions actions = new PlayerInputActions();
+        actions.PlayerControl.Jump.performed += Jump;
+        actions.PlayerControl.Movement.performed += Move;
+
+        //actions.Enable();//Bu þekilde tüm player controlu açarýz
+        actions.PlayerControl.Enable();
     }
 
-    public void Movement()
+    private void Update()
     {
-       
-
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            playerInput.SwitchCurrentActionMap("UI");//ToDo:Ýt'Ll be change control inteface.
+        }
+        if (Mouse.current.rightButton.wasPressedThisFrame)
+        {
+            playerInput.SwitchCurrentActionMap("Player Control");
+            
+            
+        }
         
-       //rb.velocity=new Vector3(newValue.x, newValue.y, newValue.z);
+    }
+    public void Submit(InputAction.CallbackContext context)
+    {
+        Debug.Log("Submit"+ context);
     }
 
 
-
-    public void Jump()
+    public void Jump(InputAction.CallbackContext context)
     {
-        
-       rb.AddForce(Vector3.up*5,ForceMode.Impulse);
+        Debug.Log("jump"+ context);
+        if (context.performed)
+        {
+            
+            rb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        }
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        Vector2 direction = context.ReadValue<Vector2>();
+
+
+        rb.velocity = new Vector3(direction.x, 0, direction.y) *5;
     }
 }
-    
